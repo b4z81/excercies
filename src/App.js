b4f7347d55b6058eb7146ef8f882ee23bfc36ba2
b4z81 <ste.guglielmi@gmail.com>
2019-06-01 10:31:05 +0200

    gestione icone meteo, cambio gestione chiamate

--- src/App.js
+++ src/App.js
@@ -3,14 +3,55 @@ import './App.scss';
 import WeahterCard from './components/weatherCard/weatherCard';
 
 class App extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentLocation: {
+                lat: undefined,
+                lng: undefined,
+            },
+            isLoading: true,
+            error: null
+        };
+    }
+
+    componentDidMount() {
+        this.findCoordinates();
+    }
+
     render() {
-        return (
-            <section className="content">
-                <div className="cards">
-                    <WeahterCard />
-                </div>
-            </section>
-        );
+        if (!this.state.isLoading) {
+            return (
+                <section className="content">
+                    <div className="cards">
+                        <WeahterCard location={this.state.currentLocation} />
+                    </div>
+                </section>
+            );
+        }
+        else {
+            return (
+                <section className="content">
+                    <div className="loadingRing"></div>
+                </section>
+            );
+        }
+    }
+
+    findCoordinates = () => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const coords = position.coords;
+
+            this.setState({
+                currentLocation: {
+                    lat: coords.latitude.toFixed(3),
+                    lng: coords.longitude.toFixed(3)
+                },
+                isLoading: false
+            });
+        },error => window.alert(error.message));
     }
 }
 
--- src/components/weatherCard/weatherCard.js
+++ src/components/weatherCard/weatherCard.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './weatherCard.scss';
 import MapBox from '../mapBox/mapBox';
 import Moment from 'moment';
-import logo from '../../images/icons/04n.svg';
-
-const apiKey = 'bc7d98df81a8ca950c00046fd18c66a0';
 
 class WeatherCard extends React.Component {
     constructor(props) {
@@ -12,95 +9,74 @@ class WeatherCard extends React.Component {
 
         this.state = {
             weather: {},
-            currentLocation: {
-                lat: undefined,
-                lng: undefined,
-            },
             isLoading: true,
             error: null
         };
     }
-
-    componentDidMount(){
-        if (navigator && navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(async pos => {
-                const coords = pos.coords;
-                
-                this.setState({
-                    currentLocation: {
-                        lat: coords.latitude.toFixed(3),
-                        lng: coords.longitude.toFixed(3)
-                    }
-                });
-
-                fetch(`http://api.openweathermap.org/data/2.5/forecast?lang=it&units=metric&lat=${this.state.currentLocation.lat}&lon=${this.state.currentLocation.lng}&appid=${apiKey}`)
-                .then(response => response.json())
-                .then(data => 
-                    this.setState({
-                        weather: data,
-                        isLoading: false}))
-                .catch(error => this.setState({ error, isLoading: false }))
-            });
-        }
+    componentDidMount() {
+        this.getWeatherFromLocation(this.props.location);
     }
 
     render() {
-        Moment.locale('it');
-        if(!this.state.isLoading) {
+        if (!this.state.isLoading) {
             return (
                 <div className="weatherCard">
-                    <div className="weatherCard__side weatherCard__side--front weatherCard__side--front-1">
+                    <div className="weatherCard__side weatherCard__side--front">
                         <div className="weatherCard__description">
                             <h3>
                                 {this.state.weather.city.name}
                             </h3>
-                            <img alt="" src={`../images/icons/${this.state.weather.list[0].weather[0].icon}.svg`} className="weatherCard__icon" />
-                            <img alt="" src={logo} className="weatherCard__icon" />
+                            <span className={"weatherIcon weatherIcon--big weatherIcon--" + this.state.weather.list[0].weather[0].icon}></span>
                             <h4>
                                 {this.state.weather.list[0].main.temp.toFixed(0)}°
                             </h4>
                             <p>{this.state.weather.list[0].weather[0].description}</p>
                             <p>Humidity: {this.state.weather.list[0].main.humidity}%</p>
-                        </div>
-                        <ul className="weatherCard__week">
+                            <ul className="weatherWeek">
                             {this.state.weather.list.map((value, index) => {
                                 if((index % 8) === 0) {
-                                    const day = value.dt_txt;
+                                    const date = value.dt_txt;
                                     return (
-                                        <li key={index}>
-                                            <span>
-                                                {Moment(day).format('L')} 
+                                        <li className={"weatherWeek__box " + (index === 0 ? 'weatherWeek__box--current' : '')} key={index}>
+                                            <span className="weatherWeek__day">
+                                                {Moment(date).format('ddd D')} 
                                             </span>
+                                            <span className={"weatherIcon weatherIcon--medium weatherIcon--" + this.state.weather.list[index].weather[0].icon}></span>
                                             <span>
-                                                {value.weather[0].icon} 
-                                            </span>
-                                            <span>
-                                                {value.main.temp_max} {value.main.temp_min}
+                                                {value.main.temp_max.toFixed(0)}° {value.main.temp_min.toFixed(0)}°
                                             </span>
                                         </li>
                                     )
                                 }
                             })}
-                        </ul>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="weatherCard__side weatherCard__side--back weatherCard__side--back-1">
+                    <div className="weatherCard__side weatherCard__side--back">
                         <div className="weatherCard__description">
-                            <MapBox location={this.state.currentLocation} />
+                            <MapBox location={this.props.location} />
                         </div>
                     </div>
                 </div>
             );
-        } else {
-            return (
-                <div className="weatherCard">
-                    <div className="weatherCard__side weatherCard__side--front">
-                        <div className="weatherCard__description">
-                            <div className="lds-dual-ring"></div>
-                        </div>
-                    </div>
-                </div>
+        }  else {
+            return (                
+                <div className="loadingRing"></div>
             );
         }
+    }
+
+    getWeatherFromLocation = (location) => {   
+        const apiKey = 'bc7d98df81a8ca950c00046fd18c66a0';         
+
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${location.lat}&lon=${location.lng}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data =>
+            this.setState({
+                weather: data,
+                isLoading: false
+            }))
+        .catch(error => this.setState({ error, isLoading: false }))
     }
 }
 
